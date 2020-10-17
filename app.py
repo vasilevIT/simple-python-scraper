04d472c4463cb11dcea52500b7278c1212814dbb
Anton Vasilev <bysslaev@gmail.com>
2020-10-17 19:50:26 +0300

    Реализация задачи во время занятия 17/10/2020.

--- app.py
+++ app.py
@@ -7,6 +7,10 @@
 
 import time
 from daemon import logger
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
 
 
 class App:
@@ -14,4 +18,55 @@ class App:
         while True:
             # Специфичная логика вашего приложения
             time.sleep(5)
-            logger.info("some info")
+            self.load_page()
+
+    def stop(self):
+        pass
+
+    def load_page(self):
+        filename = '/Users/antonvasilev/PyCharmProjects/bsbo__09_and_10__17/results.csv'
+        if os.path.isfile(filename):
+            self.store = pd.read_csv(filename)
+        else:
+            self.store = pd.DataFrame()
+
+        self.results = []
+        url = 'https://www.mirea.ru/news/'
+        response = requests.get(url)
+        html = response.text
+
+        bs = BeautifulSoup(html, 'lxml')
+        container = bs.find('div', {'class': 'uk-grid-medium'})
+        cards = container.find_all('div', {'class': 'uk-card'})
+        for card in cards:
+            self.parse_card(card)
+
+        if len(self.results) > 0:
+            df = pd.DataFrame(self.results)
+
+            result_df = pd.concat([self.store, df])
+            result_df.to_csv('/Users/antonvasilev/PyCharmProjects/bsbo__09_and_10__17/results.csv')
+
+    def parse_card(self, card):
+        img = card.find('img')
+        link = card.find('a', {'class': 'uk-link-reset'})
+
+        image_src = img.get('src')
+        href = link.get('href')
+        title = link.text
+        date = card.find('div', {'class': 'uk-h6'}).text
+
+        if not self.isExists(href):
+            self.results.append({
+                "href": href,
+                "title": title,
+                "date": date,
+                "image_src": image_src
+            })
+            logger.info('New card ' + title)
+
+    def isExists(self, href):
+        if not ('href' in self.store.columns):
+            return False
+        found = self.store[self.store['href'] == href]
+        return bool(found.count()[0])
---
+++ example.py
@@ -0,0 +1,10 @@
+"""
+  Created by PyCharm.
+  User: antonvasilev <bysslaev@gmail.com>
+  Date: 17/10/2020
+  Time: 11:23
+ """
+from app import App
+
+application = App()
+application.load_page()
